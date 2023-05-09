47a8b080aaac4520f77bececeba1ff426c39e927
Choudhary Reyan Khan <93575436+ReyanChaudhary@users.noreply.github.com>
2023-05-09 15:19:56 +0530

    Create fruitsselector.php

---
+++ fruitsselector.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="arrayimplementation.php" method="POST">
+        Apples: <input type="checkbox"  name = "fruits[]" value = "apples">
+        <br>
+        Oranges: <input type="checkbox"  name = "fruits[]" value = "oranges">
+        <br>
+        Kiwis: <input type="checkbox"  name = "fruits[]" value = "kiwis">
+        <br>
+        <input type="submit">
+    </form>
+
+    <?php 
+    //We have to initialize a variable first
+    $fruits = $_POST["fruits"];
+    echo $fruits[0];
+    ?>
+</body>
+</html>
